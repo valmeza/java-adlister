1bcf53d9734081d5e334aeaaad04869994d1a87e
Valeria Meza <valeriameza173@gmail.com>
2020-06-02 15:42:31 -0500

    feat: added the reset feature.

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -8,15 +8,21 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
 public class CounterServlet extends HttpServlet {
-    private int pageCount;
-
-    public void init() {
-        // Reset page counter
-        pageCount = 0;
-    }
+    private int pageCount = 0;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
+        try {
+            if (request.getParameter("reset").equals("1")) {
+                pageCount = 0;
+            }
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+        }
+
         pageCount++;
         String title = "Total # of page views: ";
         out.println("<h1>" + title + " " + pageCount + "</h1>");
