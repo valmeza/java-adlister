8daea65ab38d7673a1acab0f0f1682b04f686404
Valeria Meza <valeriameza173@gmail.com>
2020-06-08 13:57:46 -0500

    feat: completed first part of exercise

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean isValid = username.equals("admin") && password.equals("password");
+        if (isValid) {
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
