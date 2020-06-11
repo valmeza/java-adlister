a49f1c81c3126d13c31f27318ae610f575ac7c69
Valeria Meza <valeriameza173@gmail.com>
2020-06-11 13:50:34 -0500

    completed connecting db.

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,61 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection = null;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        String selectQuery = "SELECT * FROM ads";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(selectQuery);
+
+            while(rs.next()) {
+                ads.add(new Ad (
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+        } catch(SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        String addQuery = String.format("INSERT INTO ads (user_id, title, description) VALUES (%s, %s, %s)", ad.getUserId(), ad.getTitle(), ad.getDescription());
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(addQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+
+            if(rs.next()) {
+                return rs.getLong(1);
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return null;
+    }
+}
